d444c85f30bac27501cce316a53f35cdd087ec6b
xiaoxia <shishuaichao578@163.com>
2024-12-20 00:34:51 +0800

    初步拖拽完成

---
+++ 拖拽/sortable.js
@@ -0,0 +1,22 @@
+
+const duration = 150
+
+function css(el, prop, val) {
+  var style = el && el.style;
+  if (style) {
+    if (val === void 0) {
+      if (document.defaultView && document.defaultView.getComputedStyle) {
+        val = document.defaultView.getComputedStyle(el, '');
+      } else if (el.currentStyle) {
+        val = el.currentStyle;
+      }
+      return prop === void 0 ? val : val[prop];
+    } else {
+      if (!(prop in style) && prop.indexOf('webkit') === -1) {
+        prop = '-webkit-' + prop;
+      }
+      style[prop] = val + (typeof val === 'string' ? '' : 'px');
+    }
+  }
+}
+
